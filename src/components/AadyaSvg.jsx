585f6686e54b7efddb396fcc3968bd4e41095959
Aadya <102467098+aadya-khanna@users.noreply.github.com>
2025-12-02 22:25:31 -0500

    removed random sentences

--- src/components/AadyaSvg.jsx
+++ src/components/AadyaSvg.jsx
@@ -3,57 +3,10 @@ import aadyaLightSvg from '../assets/aadya_light.svg';
 
 export default function AadyaSvg({ className = '' }) 
 {
-    const sentences = ["Hope you're having a great day!", 
-        "Remember to drink water!", 
-        "Don't forget to write me a note!",
-         "MATH135 > MATH137", 
-         "Fun fact: I've travelled to 22 countries!",
-         "I want to go to IKEAAAA",
-         "I'm actually a really funny person",
-         "Is this an implication!?",
-         "WINGSTOP WINGSTOP WINGSTOP",
-         "I want to go to IKEAAAA"]
-
-    const randomSentence = sentences[Math.floor(Math.random() * sentences.length)]
-
     return (
         <div className="relative inline-block"> 
-            <div className="absolute top-10 md:top-20 left-1/2 -translate-x-1/2 md:left-auto md:translate-x-14 md:-right-5 bg-background dark:bg-background-dark border-2 border-foreground dark:border-foreground-dark rounded-2xl px-4 py-2 shadow-lg z-10 overflow-visible">
-                <p className="text-sm md:text-base font-plein text-foreground dark:text-foreground-dark whitespace-nowrap">
-                 {randomSentence}
-                </p>
-                
-                <div className="absolute bottom-0 left-1/2 -translate-x-1/2 md:left-4 md:translate-x-0 translate-y-2">
-                    <svg width="16" height="10" viewBox="0 0 16 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path 
-                            d="M4 0 L16 0 L8 10 Z" 
-                            fill="currentColor" 
-                            className="text-background dark:text-background-dark"
-                        />
-                        <line 
-                            x1="4" 
-                            y1="0" 
-                            x2="8" 
-                            y2="10" 
-                            stroke="currentColor" 
-                            strokeWidth="2" 
-                            strokeLinecap="round"
-                            className="text-foreground dark:text-foreground-dark"
-                        />
-                        <line 
-                            x1="8" 
-                            y1="10" 
-                            x2="16" 
-                            y2="0" 
-                            stroke="currentColor" 
-                            strokeWidth="2" 
-                            strokeLinecap="round"
-                            className="text-foreground dark:text-foreground-dark"
-                        />
-                    </svg>
-                </div>
-            </div>
             
+        
             {/* Light mode SVG - hidden in dark mode */}
             <img 
                 src={aadyaLightSvg} 
